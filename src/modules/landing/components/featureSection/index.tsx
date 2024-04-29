e5ffa12610ec60cf869fd981b3b765b7aed4c0d2
mahdiyar <mahdiyar@pro.ai>
2024-04-29 22:18:44 +0330

    add other section into landing

---
+++ src/modules/landing/components/featureSection/index.tsx
@@ -0,0 +1,16 @@
+const FeatureSection=()=>{
+    return(
+        <div className="container mt-[80px]">
+       <div className="text-center">
+          <h2 className="text-5xl leading-[150%]">
+            همه چیزی که برای ساخت <br />{" "}
+            <span className="gradientText">ربات‌های سفارشی</span> نیاز
+            دارید.
+          </h2>
+        </div>
+
+        </div>
+    )
+};
+
+export default FeatureSection;
--- src/modules/landing/index.tsx
+++ src/modules/landing/index.tsx
@@ -1,10 +1,14 @@
+import FeatureSection from "./components/featureSection"
 import IntroduceSections from "./components/introduceSections"
+import TrustBrnadSections from "./components/trustBrnadSections"
 
 const Landing = () => {
     return (
         <>
             <div>
                 <IntroduceSections />
+                <TrustBrnadSections/>
+                <FeatureSection/>
             </div>
         </>
     )
